1d4a285fe6c2aecb82e11544b0c3b68623e3e232
yaraaamohsen <0yaramohsen00@gmail.com>
2025-04-13 20:47:09 +0200

    Implementation of generic repository

---
+++ Core/Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+
+namespace Domain.Contracts
+{
+    public interface IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(bool trackChanges = false);
+        Task<TEntity?> GetByIdAsync(TKey id);
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
---
+++ Infrastructure/Presistance/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Contracts;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Presistance.Data;
+
+namespace Presistance.Repositories
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly StoreDbContext _context;
+
+        public GenericRepository(StoreDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool trackChanges = false)
+        {
+            return trackChanges ?
+                            await _context.Set<TEntity>().ToListAsync() :
+                            await _context.Set<TEntity>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(TKey id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+        
+        public async Task AddAsync(TEntity entity)
+        {
+            await _context.AddAsync(entity);
+        }
+        
+        public void Update(TEntity entity)
+        {
+            _context.Update(entity);
+        }
+        
+        public void Delete(TEntity entity)
+        {
+            _context.Remove(entity);
+        }
+
+    }
+}
