156d17dbbb8ade8514eb4a9d74ea14b2db1224df
yaraaamohsen <0yaramohsen00@gmail.com>
2025-04-20 15:49:34 +0200

    Refactoring For Program

---
+++ Store.Owner/Extensions/ExtensionsForMiddlewares.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using Persistance;
+using Shared.ErrotModels;
+using Domain.Contracts;
+using Store.Owner.Middlewares;
+
+namespace Store.Owner.Extensions
+{
+    public static class ExtensionsForMiddlewares
+    {
+        public static async Task<WebApplication> ConfigureMiddlewares(this WebApplication app)
+        {
+            await app.InitializeDatabaseAsync();
+
+            app.UseGlobalErrorHandling();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseStaticFiles();
+
+            app.MapControllers();
+
+            return app;
+        }
+
+        private static async Task<WebApplication> InitializeDatabaseAsync(this WebApplication app)
+        {
+            // Seeding
+            using var scope = app.Services.CreateScope();
+            var dbIntializer = scope.ServiceProvider.GetRequiredService<IDbIntializer>();
+            await dbIntializer.IntializeAsync();
+
+            return app;
+        }
+        
+        private static WebApplication UseGlobalErrorHandling(this WebApplication app)
+        {
+            app.UseMiddleware<GlobalErrorHandlingMiddleware>();
+
+            return app;
+        }
+    }
+}
--- Store.Owner/Program.cs
+++ Store.Owner/Program.cs
@@ -9,8 +9,8 @@ using Presistance.Data;
 using Services;
 using Services.Abstractions;
 using Shared.ErrotModels;
+using Store.Owner.Extensions;
 using Store.Owner.Middlewares;
-using AssemblyMapping = Services.AssemblyReference;
 
 namespace Store.Owner
 {
@@ -20,70 +20,12 @@ namespace Store.Owner
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Services.RegisterAllServices(builder.Configuration);
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            //builder.Services.AddDbContext<StoreDbContext>(options =>
-            //{
-            //    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            //});
-
-            //builder.Services.AddScoped<IDbIntializer, DbIntializer>();
-            //builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
-            builder.Services.AddInfrastructureServices(builder.Configuration);
-
-            //builder.Services.AddAutoMapper(typeof(AssemblyMapping).Assembly);
-            //builder.Services.AddScoped<IServiceManager, ServiceManager>();
-
-            builder.Services.AddApplicationServices();
-
-            builder.Services.Configure<ApiBehaviorOptions>(config =>
-            {
-                config.InvalidModelStateResponseFactory = (actionContext) =>
-                {
-                    var errors = actionContext.ModelState.Where(m => m.Value.Errors.Any())
-                    .Select(m => new ValidationError()
-                    {
-                        Field = m.Key,
-                        Errors = m.Value.Errors.Select(errors => errors.ErrorMessage)
-                    });
-
-                    var response = new ValidationErrorResponse()
-                    {
-                        Errors = errors
-                    };
-                    return new BadRequestObjectResult(response);
-                };
-            });
 
             var app = builder.Build();
 
-            // Seeding
-            using var scope = app.Services.CreateScope();
-            var dbIntializer = scope.ServiceProvider.GetRequiredService<IDbIntializer>();
-            await dbIntializer.IntializeAsync();
-
-            app.UseMiddleware<GlobalErrorHandlingMiddleware>();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.UseStaticFiles();
-
-            app.MapControllers();
+            await app.ConfigureMiddlewares();
 
             app.Run();
         }
